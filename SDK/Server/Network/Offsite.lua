39123024b7bfd71122c99b02c7cc31fcc6523031
Space System Test <69744068+spacesystemtesting@users.noreply.github.com>
2025-04-01 12:24:29 -0400

    Logger

--- SDK/Server/Network/Offsite.lua
+++ SDK/Server/Network/Offsite.lua
@@ -1,8 +1,10 @@
 local HttpService = game:GetService("HttpService")
 
-local Config = (script.Parent.SDK).Config
+local Config = require(script.Parent.SDK).Config
 
 local Offsite = {}
+
+local Logger = require(script.Parent.Utils.Logger)
 
 function Offsite:Ping(endpoint, data)
     local success, response = pcall(function()
@@ -15,14 +17,13 @@ function Offsite:Ping(endpoint, data)
     end)
 
     if success then
-        if Config.ALLOW_LOGS then
-           print("[SDK] Outbound request sent with 202, endpoint: " .. endpoint .. " data: " .. data)
-        end
+        
+        Logger:Log("Outbound request sent with 202, endpoint: " .. endpoint .. " data: " .. data)
 
         return response -- // Wondering if I should decode here or just do it in the caller
 
     else
-        print("[SDK] Outbound request sent with error, endpoint: " .. endpoint .. " data: " .. data)
+        Logger:Warn("Outbound request sent with error, endpoint: " .. endpoint .. " data: " .. data)
     end
 
 end
