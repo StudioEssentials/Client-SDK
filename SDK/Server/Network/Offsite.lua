549dcdd040583a4370b828fd9ffacc0fa843bf2f
Space System Test <69744068+spacesystemtesting@users.noreply.github.com>
2025-04-01 12:13:04 -0400

    Finished the offsite module

--- SDK/Server/Network/Offsite.lua
+++ SDK/Server/Network/Offsite.lua
@@ -6,8 +6,24 @@ local Offsite = {}
 
 function Offsite:Ping(endpoint, data)
     local success, response = pcall(function()
-        return HttpService
+        return HttpService:PostAsync(
+            Config.BACKEND .. endpoint,
+            HttpService:JSONEncode(data),
+            Enum.HttpContentType.ApplicationJson
+            {[]"Authorization"] = "Bearer " .. Config.API_KEY}
+        )
     end)
+
+    if success then
+        if Config.ALLOW_LOGS then
+           print("[SDK] Outbound request sent with 202, endpoint: " .. endpoint .. " data: " .. data)
+        end
+
+        return response -- // Wondering if I should decode here or just do it in the caller
+
+    else
+        print("[SDK] Outbound request sent with error, endpoint: " .. endpoint .. " data: " .. data)
+    end
 
 end
 
